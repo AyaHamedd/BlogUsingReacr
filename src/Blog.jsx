00be16e530af8b2bd3203321eba0afe759dbc62d
AyaHamedd <ayahamedabdulaziz@gmail.com>
2021-03-20 23:18:28 +0200

    Getting user from user id

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -16,25 +16,34 @@ function Blog() {
   const [state, updateState] = useState({
     loading: true,
     posts: [],
-    users: []
+    users: [],
   });
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => updateState({posts: json, loading: false}));
+      .then((json) => updateState({ posts: json, loading: false }));
     console.log(state);
 
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((json) => updateState({users: json, loading: false}));
+      .then((response) => response.json())
+      .then((json) => updateState({ users: json, loading: false }));
   }, []);
 
   return (
     <div>
-      {state.loading ? <p>Loading...</p> :
-      state.posts.map((post) => (
-        <PostCard post={post}></PostCard>
-      ))}
+      {state.loading ? (
+        <p>Loading...</p>
+      ) : (
+        state.posts.map((post) => {
+          // state.users.filter((user) => user.id == post.userId);
+          state.users.map((user) => {
+            if (user.id === post.userId) {
+              return post.assign(user);
+            }
+          });
+          return <PostCard post={post} user={state}></PostCard>;
+        })
+      )}
     </div>
   );
 }
