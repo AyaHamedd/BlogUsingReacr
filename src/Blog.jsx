88c262396dbff04b5540b0a23c1227f8bfff4867
Heba Mostafa <Hoba.mostafa97@gmail.com>
2021-03-20 22:32:56 +0200

    Complete render of blog

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,33 +1,36 @@
 // 1.Blog. has many Post Cards
 // 2.PostCard.
 
-import { useEffect } from "react";
-import {useState}    from "react";
+import { useEffect, useState } from "react";
 
-function PostCard( {post}) {
-  return <div><h2>post.title</h2><p>post.body</p></div>;
+function PostCard({ post }) {
+  return (
+    <div>
+      <h2>post.title</h2>
+      <p>post.body</p>
+    </div>
+  );
 }
 
 function Blog() {
-
   const [state, updateState] = useState({
     loading: false,
     posts: [],
     // users: []
   });
   useEffect(() => {
-  fetch("https://jsonplaceholder.typicode.com/posts").then(
-    (response) => response.json()
-  ).then((json) => (
-    updateState(json)
-  ))
-console.log(state)
-  }) ;
-   return(<h1>leena</h1>);
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => updateState(json));
+    console.log(state);
+  }, [state.posts]);
+  return (
+    <div>
+      {state.posts.map((post) => (
+        <PostCard post={post}></PostCard>
+      ))}
+    </div>
+  );
 }
-  
-  
-
-
 
 export default Blog;
