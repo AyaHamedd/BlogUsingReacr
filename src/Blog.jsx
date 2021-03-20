26467befce5b0e2c8c271597d41869af1d978bd0
leenasherif239 <sherifleena@yahoo.com>
2021-03-20 22:19:04 +0200

    addRenderBlog

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,9 +2,10 @@
 // 2.PostCard.
 
 import { useEffect } from "react";
+import {useState}    from "react";
 
-function PostCard() {
-  return <div></div>;
+function PostCard( {post}) {
+  return <div><h2>post.title</h2><p>post.body</p></div>;
 }
 
 function Blog() {
@@ -12,20 +13,21 @@ function Blog() {
   const [state, updateState] = useState({
     loading: false,
     posts: [],
-    users: []
+    // users: []
   });
   useEffect(() => {
   fetch("https://jsonplaceholder.typicode.com/posts").then(
     (response) => response.json()
   ).then((json) => (
-    updateState({ json })
+    updateState(json)
   ))
-
-  })
+console.log(state)
+  }) ;
+   return(<h1>leena</h1>);
 }
   
   
-}
 
 
-export default App;
+
+export default Blog;
