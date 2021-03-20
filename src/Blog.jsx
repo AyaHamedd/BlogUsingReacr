bbc06d5e43c8ee6ad78ad3ddcae1205db0cd143a
Abobakr <abobakryousre@gmail.com>
2021-03-20 22:01:41 +0200

    updated function useEffect in blog component

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,22 +1,31 @@
 // 1.Blog. has many Post Cards
 // 2.PostCard.
 
+import { useEffect } from "react";
+
 function PostCard() {
   return <div></div>;
 }
 
 function Blog() {
+
   const [state, updateState] = useState({
     loading: false,
     posts: [],
     users: []
   });
+  useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (response) => response.json()
+  ).then((json) => (
+    updateState({ json })
+  ))
 
-  useEffect(()=>
-  fetch("https://jsonplaceholder.typicode.com/posts")
-  .then( (response)=> response.json())
-  return (
-  );
+  })
 }
+  
+  
+}
+
 
 export default App;
