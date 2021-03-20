6c486ece3b18b05128376ced9aa9d0b2bf285046
SarahOuf <sarah.m.ouf@gmail.com>
2021-03-20 22:48:02 +0200

    add loading state

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,27 +6,28 @@ import { useEffect, useState } from "react";
 function PostCard({ post }) {
   return (
     <div>
-      <h2>post.title</h2>
-      <p>post.body</p>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
     </div>
   );
 }
 
 function Blog() {
   const [state, updateState] = useState({
-    loading: false,
+    loading: true,
     posts: [],
     // users: []
   });
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => updateState(json));
+      .then((json) => updateState({posts: json, loading: false}));
     console.log(state);
   }, [state.posts]);
   return (
     <div>
-      {state.posts.map((post) => (
+      {state.loading ? <p>Loading...</p> :
+      state.posts.map((post) => (
         <PostCard post={post}></PostCard>
       ))}
     </div>
